dae7b4b2bd7499e3d5022113c0885754b93a6502
Dmatt547 <162403766+Dmatt547@users.noreply.github.com>
2024-03-14 21:16:28 +1100

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Dan"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "dan1234567"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2467214			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "B1866QBUVCHMRFVS"   // replace XYZ with your channel write API Key
